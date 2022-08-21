20c7e6116f9be7b014af5d4148c59287134badac
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-08-21 23:41:02 +0900

    test: 💍 flutter test と flutter analyze を GitHub Actions で実行する

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,19 +1,14 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:account_memories/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('文字列', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Amazon'), findsOneWidget);
+    expect(find.text('楽天'), findsOneWidget);
+    expect(find.text('Yahoo!'), findsOneWidget);
+    expect(find.text('Google'), findsNothing);
+    expect(find.text('Microsoft'), findsNothing);
   });
 }
